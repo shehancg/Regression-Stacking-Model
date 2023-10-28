c9f43fd5a9af327b7d42ae2b30be1d4d75b6cf4e
Shehan Gunawardena <64646307+shehancg@users.noreply.github.com>
2023-10-29 00:08:54 +0530

    Added meal plan

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pickle
+import pandas as pd
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -6,6 +7,9 @@ app = Flask(__name__)
 # Load the stacking model from the pickle file
 with open('stacking_model.pkl', 'rb') as file:
     stacking_model = pickle.load(file)
+
+# Load the Excel file with BodyTypes and meals
+meals_df = pd.read_excel('MealPlan.xlsx')  # Replace 'meals_data.xlsx' with the actual file path
 
 def classify_body_type(bmi, fat_percentage):
     if bmi < 18.5 and fat_percentage < 20:
@@ -47,7 +51,23 @@ def predict():
         prediction = stacking_model.predict([input_data])[0]
         body_type = classify_body_type(bmi, prediction)
 
-        result = {'Fat percentage': prediction, 'BMI':bmi, 'Body Type': body_type}
+        # Print the column names for reference
+        print(meals_df.columns)
+        
+        # Retrieve meals for the BodyType from the DataFrame
+        meals_data = meals_df[meals_df['BodyTypes'] == body_type]
+        meals_list = meals_data.to_dict(orient='records')
+        
+        # Rearrange the keys for the result dictionary
+        result = {
+            "BMI": bmi,
+            "Body Type": body_type,
+            "Fat percentage": prediction,
+            "Breakfast": meals_list[0]['Breakfast'],
+            "Lunch": meals_list[0]['Lunch'],
+            "Dinner": meals_list[0]['Dinner']
+        }
+
         return jsonify(result)
     except Exception as e:
         return jsonify({'error': str(e)})
